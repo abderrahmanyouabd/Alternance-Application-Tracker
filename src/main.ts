d33206a7113f2281b2ab07ae19d86b7048297e96
abderrahmanyouabd <youabd50@gmail.com>
2025-01-30 14:33:36 +0100

    refactor: update main app component to use router and nav bar

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import { JobTrackerComponent } from './app/components/job-tracker/job-tracker.component';
+import { provideRouter, RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './app/components/nav-bar/nav-bar.component';
+import { FooterComponent } from './app/components/footer/footer.component';
+import { routes } from './app/app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [JobTrackerComponent],
-  template: '<app-job-tracker></app-job-tracker>'
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
+  template: `
+    <div class="min-h-screen flex flex-col">
+      <app-nav-bar></app-nav-bar>
+      <main class="flex-grow">
+        <router-outlet></router-outlet>
+      </main>
+      <app-footer></app-footer>
+    </div>
+  `
 })
 export class App {}
 
 bootstrapApplication(App, {
   providers: [
-    provideHttpClient()
+    provideHttpClient(),
+    provideRouter(routes)
   ]
 });
